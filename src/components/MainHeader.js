a2fa50a8b31868ad42846adfd6e17cc64a14c7a3
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-11-05 16:54:32 +0200

    commit for navlink activepath

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -2,18 +2,18 @@ import { NavLink } from 'react-router-dom';
 
 import classes from './MainHeader.module.css';
 
-const MainHeader = () => {
+const MainHeader = () => {  
   return (
     <header className={classes.header}>
       <nav>
         <ul>
           <li>
-            <NavLink activeClassName={classes.active} to='/welcome'>
+            <NavLink className={(navData) =>  navData.isActive ? classes.active : '' } to='/welcome'>
               Welcome
             </NavLink>
           </li>
           <li>
-            <NavLink activeClassName={classes.active} to='/products'>
+            <NavLink className={(navData) => navData.isActive ? classes.active : '' } to='/products'>
               Products
             </NavLink>
           </li>
